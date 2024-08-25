aaabc5297db79052bb38c9a54b6526f2d3fe85ab
Noxanoxa <amrisamer54@gmail.com>
2024-08-25 19:22:47 +0100

    ADD: Tags

---
+++ mindcms-blog/app/Models/Tag.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+    use Sluggable;
+
+    protected $guarded =[];
+
+    public function sluggable(): array
+    {
+    return [
+        'slug' => [
+            'source' => 'name',
+        ],
+    ];
+    }
+
+    public function  posts()
+    {
+        return $this->belongsToMany(Tag::class, 'posts_tags');
+    }
+
+}
